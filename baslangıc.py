94acd74e5a24431a5f172867d4fe6d65b8b95445
rumeysayilma <75755454+rumeysayilma@users.noreply.github.com>
2021-01-16 16:18:46 +0300

    2.gün

--- baslangıc.py
+++ baslangıc.py
@@ -4,10 +4,19 @@ from matplotlib import pyplot as plt
 import random
 import math
 import random
+from tqdm import tqdm
 
 """ Generating Billing Data. """
 y_d = [0, 0]
 x = []
+
+a1=[2,3,4]
+a2=[1,2,3]
+a1=np.array(a1)
+a2=np.array(a2)
+x.append(a1)
+x.append(a2)
+
 
 for i in range(2, 100):
     a = []
@@ -38,17 +47,77 @@ def datayi_karistir_test_ve_egitimi_ayir(x, y):
 
 x_egitim, x_test, yd_egitim, yd_test = datayi_karistir_test_ve_egitimi_ayir(
     x, y_d)
+print('y_d')
+print(np.shape(y_d))
+print('x')
+print(np.shape(x))
 
 x_egitim = data_vectorizer(x_egitim, 3)
-'''
+
+""" 
 yd_test = data_vectorizer(yd_test, 1)
-yd_egitim = data_vectorizer(yd_egitim, 1)
-'''
+yd_egitim = data_vectorizer(yd_egitim, 1) 
+"""
+
 x_test = data_vectorizer(x_test, 3)
 
+def v_to_x(v):
+    
+    return v
+
+def derivative_v_to_x(v):
+    return v
+
+weights_u = np.random.randn(7, 3) 
+weights_x = np.random.randn(7, 7) 
+weights_y = np.random.randn(1, 7) 
+epoch = 1200
+learning_rate = 0.5
+epoch_iterator = tqdm(range(epoch))
+
+for e in range(1200):
+    for i, (u, yd) in enumerate(zip(x_egitim, yd_egitim)):
+
+        #forward
+        if e==0 and i==0:
+            x_k = np.random.randn(7,1)
+        
+        V_k = np.dot(weights_x, x_k) + np.dot(weights_u, u)
+        x_k = v_to_x(V_k)
+        y_k = np.dot(weights_y , x_k)
+
+        #hata
+        e = yd - y_k
+        E = 0.5 * e.T * e
+        """         
+        print('yd')
+        print((yd))
+        print('y_k')
+        print(np.shape(y_k))        
+        
+       
+        print('weights_y.T')
+        print(np.shape(weights_y.T))
+        print('********')
+        print('e')
+        print(np.shape(e))
+        print('********')   
+        print('np.dot(weights_y.T, e)')
+        print(np.shape(np.dot(weights_y.T, e)))
+        print('********')  
+        print('derivative_v_to_x(V_k)')
+        print(np.shape(derivative_v_to_x(V_k)))
+        print('********')   
+        print('x_k.T')
+        print(np.shape(x_k.T))
+        print('********')  
+        """     
+        
+        #agırlık güncellenmesi
+        weights_x = weights_x + learning_rate * np.dot(((np.dot(weights_y.T, e))*derivative_v_to_x(V_k)), x_k.T)
+        weights_u = weights_u + learning_rate * np.dot(((np.dot(weights_y.T, e))*derivative_v_to_x(V_k)), u.T)
+        weights_y = weights_y + learning_rate * e * x_k.T
+        print(e)
 
 
-weights_u = np.random.randn(2, 3) 
-weights_x = np.random.randn(2, 2) 
-weights_y = np.random.randn(1, 2) 
-
+        
